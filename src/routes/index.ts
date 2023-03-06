2ef745359bd623137e8bc492fe9c43e9fa715887
MisterBeamCs <103748132+BrunoRRamos@users.noreply.github.com>
2023-03-06 13:12:50 -0300

    fix:

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,8 @@
 import express from "express";
 
 const routes = (app: any) => {
-    app.route('/').get((req: any, res: any) => {
-        res.status(200).send({title: "Batata"})
+    app.route('/').get((req: Request, res: any) => {
+        res.status(200).send({ title: "Batata" })
     });
 
     app.use(
